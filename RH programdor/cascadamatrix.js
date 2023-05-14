d949c5353ddf03d225d88ea35e4879bc5116f30d
Engineerdog <133551521+Engineerdog@users.noreply.github.com>
2023-05-13 21:13:46 -0500

    Mi primera pagina

---
+++ RH programdor/cascadamatrix.js
@@ -0,0 +1,52 @@
+const canvas = document.getElementById("canvas"); 
+const ctx = canvas.getContext('2d'); 
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight; 
+const arrayCaracteres = [
+    "a", "b", "c", "d", "e", "f", "g", "h", "i", "j",
+    "k", "l", "m", "n", "ñ", "o", "p", "q", "r", "s",
+    "t", "u", "v", "w", "x", "y", "z", "£", "¥", "§",
+    "¤","€","$","±","µ","Ø","×","≥","Æ","☠"
+];
+
+const codigoArray = []; 
+const conteoCodigo = 100;
+const fontSize = 18;
+const numeroColumna = canvas.width / fontSize;
+let frame = 0;
+class Matrix {
+    constructor(x, y) {
+        this.y = y
+        this.x = x
+    }
+    dibujar(ctx) {
+        this.valor = arrayCaracteres[Math.floor(Math.random() * (arrayCaracteres.length - 1))].toUpperCase()
+        this.velocidad = Math.random() * fontSize * 3 / 4 + fontSize * 3 / 4
+        ctx.fillStyle = "rgba(0,255,0)"
+        ctx.font = fontSize + "px comic-sans" 
+        ctx.fillText(this.valor, this.x, this.y) 
+        this.y += this.velocidad
+        if (this.y > canvas.height) {
+            this.x = Math.floor((Math.random() * numeroColumna) * fontSize)
+            this.y = 0
+            this.velocidad = (-Math.random() * fontSize * 3) / 4 + (fontSize * 3) / 4
+        }
+    }
+}
+
+let actualizar = () => {
+    if (codigoArray.length < conteoCodigo) { 
+        let matrix = new Matrix(Math.floor(Math.random() * numeroColumna) * fontSize, 0) 
+        codigoArray.push(matrix) 
+    }
+    ctx.fillStyle = "rgba(0,0,0,0.05)" 
+    ctx.fillRect(0, 0, canvas.width, canvas.height) 
+    for (let i = 0; i < codigoArray.length && frame % 2 == 0; i++) {
+        codigoArray[i].dibujar(ctx) 
+    }
+    requestAnimationFrame(actualizar)
+    frame++
+}
+actualizar(); 
+
+
