965bf0713a8e9aad7eb63f5602a635972f89e625
Engineerdog <133551521+Engineerdog@users.noreply.github.com>
2023-05-18 19:42:12 -0500

    paginavf

--- app.js
+++ app.js
@@ -47,6 +47,26 @@ let actualizar = () => {
     requestAnimationFrame(actualizar)
     frame++
 }
-actualizar(); 
+
+actualizar();
+
+let listElements = document.querySelectorAll('.list_button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+
+    })
+});
+
 
 
